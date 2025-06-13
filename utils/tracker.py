9312d8c3196f8405883753c497cafc40fa268506
Gopu-coder <Gopabandhudora94@gmail.com>
2025-06-13 17:42:28 +0530

    Update tracker.py

--- utils/tracker.py
+++ utils/tracker.py
@@ -1,1 +1,14 @@
 # Daily tracker input handler
+# utils/tracker.py
+import json
+from datetime import datetime
+
+def log_user_data(name, dob, result):
+    data = {
+        "timestamp": datetime.now().isoformat(),
+        "name": name,
+        "dob": dob,
+        "result": result
+    }
+    with open("user_logs.json", "a") as f:
+        f.write(json.dumps(data) + "\n")
